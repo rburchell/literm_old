a12977727a82caa30b62fc699563ccfe5a9f3129
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-04-19 22:29:38 +0200

    Remove macOS from CI and specify the number of make jobs
    Speed up builds.
    
    Issue: #20

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,3 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sudo apt-get -y install uuid-dev xvfb qt58base qt58declarative
 fi
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew install qt5
-    brew link qt5 --force
-fi
